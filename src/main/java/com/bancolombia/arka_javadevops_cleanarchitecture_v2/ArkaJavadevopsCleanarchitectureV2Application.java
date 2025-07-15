178c13bb0ae5b452822776dd012e1d71b22f2d09
Daniel Lopez <daniellop199731@gmail.com>
2025-07-15 14:09:31 -0500

    Init proyect

---
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v2/ArkaJavadevopsCleanarchitectureV2Application.java
@@ -0,0 +1,13 @@
+package com.bancolombia.arka_javadevops_cleanarchitecture_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaJavadevopsCleanarchitectureV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaJavadevopsCleanarchitectureV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancolombia/arka_javadevops_cleanarchitecture_v2/ArkaJavadevopsCleanarchitectureV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.arka_javadevops_cleanarchitecture_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaJavadevopsCleanarchitectureV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
