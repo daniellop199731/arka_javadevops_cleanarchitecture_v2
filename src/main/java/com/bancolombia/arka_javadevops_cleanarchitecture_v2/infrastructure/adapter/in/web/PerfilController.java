760ed404ca14a0a7dcb26581ca7bfb83bc0b37fe
Daniel Lopez <daniellop199731@gmail.com>
2025-07-16 19:56:06 -0500

    Correcciones

--- src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v2/infrastructure/adapter/in/web/PerfilController.java
+++ src/main/java/com/bancolombia/arka_javadevops_cleanarchitecture_v2/infrastructure/adapter/in/web/PerfilController.java
@@ -16,8 +16,6 @@ import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/perfiles")
